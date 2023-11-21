e77e7648f25cbd5ca11200b35d1909612d21573b
Hicham <hicham@Hichams-MacBook-Pro.local>
2023-11-21 11:13:58 +0100

    first commit

---
+++ app/src/main/java/com/example/stockifi/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.stockifi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+buildscript {
+    val agp_version by extra("4.0.0")
+    val agp_version1 by extra("7.2.0")
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "7.2.0" apply false
+}
