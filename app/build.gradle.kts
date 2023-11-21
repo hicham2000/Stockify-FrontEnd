b7a523682c5913f2cf66a7197b3b89ab9aa797a0
Hicham <hicham@Hichams-MacBook-Pro.local>
2023-11-21 12:43:21 +0100

    test api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,5 +38,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.jakewharton:butterknife:10.2.3")
     annotationProcessor ("com.jakewharton:butterknife-compiler:10.2.3")
+    implementation("com.android.volley:volley:1.2.1")
 
 }
--- app/src/main/java/com/example/stockifi/MainActivity.java
+++ app/src/main/java/com/example/stockifi/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.stockifi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String url = "http://10.0.2.2:1111/produits";
+
 
 
 
@@ -15,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myTextView = findViewById(R.id.mytext);
+
+
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        myTextView.setText(response.toString());
+                      //Log.e("api","OnError : " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+                Log.e("api","OnError : " + error.getLocalizedMessage());
+            }
+        });
+
+        queue.add(stringRequest);
     }
+
+
 }
