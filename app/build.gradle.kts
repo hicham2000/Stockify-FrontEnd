f5631b79892fce5d2202170917420ed59e07480e
DELL <wassim.rifay@uit.ac.ma>
2023-12-22 11:31:10 +0100

    remove RegisterInstrumentedTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.stockifi"
         minSdk = 21
-        targetSdk = 21
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
