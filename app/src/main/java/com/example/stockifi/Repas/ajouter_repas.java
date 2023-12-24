fe5223bb3ab3e8b66c720ddbdb9728ba382b4eb4
Hicham <hicham@Hichams-MacBook-Pro.local>
2023-12-24 18:34:53 +0100

    add ajouter repas api connexion

--- app/src/main/java/com/example/stockifi/Repas/ajouter_repas.java
+++ app/src/main/java/com/example/stockifi/Repas/ajouter_repas.java
@@ -7,14 +7,28 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.stockifi.Liste_Course.Produit;
 import com.example.stockifi.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class ajouter_repas extends AppCompatActivity {
     private LinearLayout container;
@@ -30,6 +44,56 @@ public class ajouter_repas extends AppCompatActivity {
         ArrayList<Produit> productList =intent.getParcelableArrayListExtra("selectedItems");
         System.out.println(productList);
         System.out.println(quantity);
+
+        Spinner spinner = findViewById(R.id.spinner_options);
+
+        String url = "http://10.0.2.2:1111/categorieDeProduits";
+        List<String> itemList = new ArrayList<>();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonResponse = new JSONArray(response);
+                            for (int i = 0; i < jsonResponse.length(); i++) {
+                                JSONObject jsonObject = jsonResponse.getJSONObject(i);
+                                itemList.add(jsonObject.getString("intitule"));
+                            }
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(ajouter_repas.this, android.R.layout.simple_spinner_item, itemList);
+
+// Specify the layout to use when the list of choices appears
+                            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+// Apply the adapter to the spinner
+                            spinner.setAdapter(adapter);
+
+
+
+
+
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                itemList.add("That didn't work!");
+
+
+            }
+        });
+
+        queue.add(stringRequest);
+
+
+
+
 
 
         for(int i =0; i<productList.size();i++){
