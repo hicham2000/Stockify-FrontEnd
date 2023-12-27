a6a3bc6c27327e5e82e45ab6b4f0ab627d9aad53
marouane <marouane.qaffssaoui@uit.ac.ma>
2023-12-27 22:07:12 +0100

    je cree la fonction ajouter catécorie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,12 +37,16 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
     implementation("androidx.test:core:1.5.0")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     testImplementation("junit:junit:4.13.2")
+    implementation("androidx.annotation:annotation:1.4.0")
     testImplementation("org.mockito:mockito-core:3.12.4")
     testImplementation("org.mockito:mockito-android:3.12.4")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
