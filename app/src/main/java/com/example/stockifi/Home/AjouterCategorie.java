647d11281ad697e23d301638fdd99d82019d58a0
marouane <marouane.qaffssaoui@uit.ac.ma>
2023-12-27 18:29:19 +0100

    je cree l'activiter Ajouter Categorie

---
+++ app/src/main/java/com/example/stockifi/Home/AjouterCategorie.java
@@ -0,0 +1,16 @@
+package com.example.stockifi.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.stockifi.R;
+
+public class AjouterCategorie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_categorie);
+    }
+}
