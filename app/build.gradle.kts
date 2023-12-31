716b71182d600e47c3c69db4f1dd9eec0aaf96ab
unknown <houssam_abouelhaoul@hotmail.fr>
2023-12-31 13:47:55 +0100

    fixed backend issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,6 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
@@ -46,9 +44,9 @@ dependencies {
     implementation("androidx.test:core:1.5.0")
     implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
     testImplementation("junit:junit:4.13.2")
-    implementation("androidx.annotation:annotation:1.4.0")
-    testImplementation("org.mockito:mockito-core:3.12.4")
-    testImplementation("org.mockito:mockito-android:3.12.4")
+    implementation("androidx.annotation:annotation:1.7.1")
+    testImplementation("org.mockito:mockito-core:5.8.0")
+    testImplementation("org.mockito:mockito-android:5.8.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
