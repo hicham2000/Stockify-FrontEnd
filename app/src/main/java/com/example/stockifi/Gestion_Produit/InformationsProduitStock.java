780caf9ce993ecf3803d5eb5ad0e999f84d276e1
unknown <houssam_abouelhaoul@hotmail.fr>
2024-01-04 20:26:56 +0100

    ajout de modifier produit et informations produits design a changer apres

---
+++ app/src/main/java/com/example/stockifi/Gestion_Produit/InformationsProduitStock.java
@@ -0,0 +1,16 @@
+package com.example.stockifi.Gestion_Produit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.stockifi.R;
+
+public class InformationsProduitStock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informations_produit_stock);
+    }
+}
