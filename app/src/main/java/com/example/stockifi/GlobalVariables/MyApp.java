6b2b9161e9b72887be3de66a57b574bc4b0f89fc
essa0ba1 <bakh2001ssa@gmail.com>
2024-01-11 15:52:11 +0100

    front end notification setup

--- app/src/main/java/com/example/stockifi/GlobalVariables/MyApp.java
+++ app/src/main/java/com/example/stockifi/GlobalVariables/MyApp.java
@@ -70,7 +70,6 @@ public class MyApp extends Application {
     public String getToken(){
         return this.token;
     }
-
     public void setToken(String token) {
         this.token = token;
     }
